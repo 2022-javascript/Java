f733ed7a674ee694c015e1206b96eb133153e00b
bigstone <sds7150sds@gmail.com>
2022-08-06 00:02:16 +0900

    첫 번째 커밋

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$server="localhost3306";
+$user="root";
+$password="smartk1";
+$dbname="opentutorials";
+
+$conn=new mysqli($server,$user,$password,$dbname);
+
+if($conn->connect_error)echo "<h2>접속에 실패하였습니다.</h2>";
+else echo "<h2>접속에 성공하였습니다</h2>";
+
+?>
